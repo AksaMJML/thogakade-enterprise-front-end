d2c5a5ff401ceb0d955141e34fb1bcbf018a22c0
AksaMJML <akaks0186@gmail.com>
2026-02-19 11:33:11 +0530

    refactor customer management page to enhance data display and structure

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -1,11 +1,26 @@
+import { NgForOf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
 export class Customer {
 
+  customerList: any = null;
+
+  constructor(private http: HttpClient) {
+    this.getAll();
+  }
+
+  getAll(){
+    this.http.get('http://localhost:8080/customer/getAll').subscribe(data => {
+      console.log(data);
+      this.customerList = data;
+    });
+  }
+  
 }
---
+++ src/model/type.ts
@@ -0,0 +1,11 @@
+export interface CustomerModel{
+    id: String;
+    title: string;
+    custName: string;
+    dob: object;
+    salary: number;
+    address: string;
+    city: string;
+    province: string;
+    postalCode: string;
+}
