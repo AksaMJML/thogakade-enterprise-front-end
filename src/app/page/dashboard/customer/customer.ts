ca5b800160238ae2033d84fdc5589ee89042a1df
AksaMJML <akaks0186@gmail.com>
2026-02-19 14:14:32 +0530

    refactor customer component to enhance form binding and structure

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -1,23 +1,41 @@
-import { NgForOf } from '@angular/common';
+import { CommonModule, NgForOf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CustomerModel } from '../../../../model/type';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
-  imports: [NgForOf],
+  imports: [NgForOf , ReactiveFormsModule, FormsModule , CommonModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
 export class Customer implements OnInit{
 
   customerList: Array<CustomerModel> = [];
+  customerObj : CustomerModel = {
+    id: '',
+    title: '',
+    name: '',
+    dob: {},
+    salary: 0.0,
+    address: '',
+    city: '',
+    province: '',
+    postalCode: ''
+  }
 
   constructor(private http: HttpClient , private cdr:ChangeDetectorRef) {
     
   }
   ngOnInit(): void {
     this.getAll();
+  }
+
+  addCustomer(){
+    // this.http.post("http:localhost:8080/customer/add").subscribe(data => {
+
+    // })
   }
 
   getAll(){
