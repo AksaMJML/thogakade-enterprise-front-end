eafe7e142241b6fdd3858e7a6cdff75e45049413
AksaMJML <akaks0186@gmail.com>
2026-02-25 08:53:47 +0530

    retrive datas in input fields but coul not load dob

--- src/model/type.ts
+++ src/model/type.ts
@@ -1,11 +1,18 @@
+export interface DobModel {
+  month: number;
+  day: number;
+  year: number;
+}
+
 export interface CustomerModel{
     id: String;
     title: string;
     name: string;
-    dob: object;
+    dob: DobModel;
     salary: number;
     address: string;
     city: string;
     province: string;
     postalCode: string;
 }
+
